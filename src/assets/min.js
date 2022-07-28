61bb7f7172de7d972c0b78126b0da5ecd7fd90f3
Nikhil <107390052+Nikhilyadav777@users.noreply.github.com>
2022-07-28 23:16:39 +0530

    Add files via upload

---
+++ src/assets/min.js
@@ -0,0 +1,17 @@
+function fun() {
+let panelsElement = document.querySelectorAll('.panel');
+
+
+let removeActiveClasses = () => {
+    panelsElement.forEach(panel => {
+        panel.classList.remove('active');
+    });
+};
+
+panelsElement.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+})};
+fun()
